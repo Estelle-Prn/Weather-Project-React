2410cf7690671be9cacf44038b8020819768fec9
Estelle-Prn <peroni.estelle@gmail.com>
2025-02-11 17:49:04 +0100

    added html in weather file

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,44 @@
 import React, { useState } from "react";
 
-export default function Weather() {}
+export default function Weather() {
+  return (
+    <div className="Search-Show">
+      <header>
+        <form className="search-block">
+          <input
+            type="search"
+            placeholder="Enter a City..."
+            required
+            className="search-text"
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+      </header>
+      <main>
+        <div className="Weather-Content">
+          <div>
+            <h1 className="City-name"></h1>
+            <p className="City-details">
+              <span></span>, <span></span>
+              <br />
+              Humidity: <strong>%</strong>, Wind:
+              <strong>km/h</strong>
+            </p>
+          </div>
+          <div className="City-temperature">
+            <span>
+              <img src="" className="temperature-icone" />
+            </span>
+            <span className="actual-temperature">
+              {Math.round(weather.temperature)}
+            </span>
+            <span className="temperature-celsius">
+              <strong> Â°C </strong>
+            </span>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+      </main>
+    </div>
+  );
+}
